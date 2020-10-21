293ba75dd67895b64d453cde4cd30e720ea7e6a6
RebeccaSvensson <rebeccasvensson.1996@gmail.com>
2020-10-21 11:06:53 +0200

    custom yolo class added, train updated to only save if better map

--- models-original.py
+++ models-original.py
@@ -12,6 +12,7 @@ from utils.utils import build_targets, to_cpu, non_max_suppression
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+import time
 
 def create_modules(module_defs):
     """
@@ -111,74 +112,72 @@ class YOLOLayer(nn.Module):
         self.anchors = anchors
         self.num_anchors = len(anchors)
         self.num_classes = num_classes
-        self.ignore_thres = 0.6
-        self.mae_loss = nn.L1Loss()
+        self.ignore_thres = 0.5
+        self.mse_loss = nn.MSELoss()
         self.bce_loss = nn.BCELoss()
         self.obj_scale = 1
         self.noobj_scale = 100
         self.metrics = {}
         self.img_dim = img_dim
+        self.grid_size = 0  # grid size
 
     def compute_grid_offsets(self, grid_size, cuda=True):
-        if cuda:
-            FloatTensor = torch.cuda.FloatTensor
-        else:
-            FloatTensor = torch.FloatTensor
-
-        stride = self.img_dim / grid_size
-
-        # Create grids
-        grid_range = torch.arange(grid_size).repeat(grid_size, 1)
-        grid_x = grid_range.view([1, 1, grid_size, grid_size]).type(FloatTensor)
-        grid_y = grid_range.t().view([1, 1, grid_size, grid_size]).type(FloatTensor)
-
+        self.grid_size = grid_size
+        g = self.grid_size
+        FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        self.stride = self.img_dim / self.grid_size
         # Calculate offsets for each grid
-        scaled_anchors = FloatTensor([(a_w / stride, a_h / stride) for a_w, a_h in self.anchors])
-        anchor_w = scaled_anchors[:, 0:1].view((1, self.num_anchors, 1, 1))
-        anchor_h = scaled_anchors[:, 1:2].view((1, self.num_anchors, 1, 1))
-        return stride, grid_x, grid_y, scaled_anchors, anchor_w, anchor_h
+        self.grid_x = torch.arange(g).repeat(g, 1).view([1, 1, g, g]).type(FloatTensor)
+        self.grid_y = torch.arange(g).repeat(g, 1).t().view([1, 1, g, g]).type(FloatTensor)
+        self.scaled_anchors = FloatTensor([(a_w / self.stride, a_h / self.stride) for a_w, a_h in self.anchors])
+        self.anchor_w = self.scaled_anchors[:, 0:1].view((1, self.num_anchors, 1, 1))
+        self.anchor_h = self.scaled_anchors[:, 1:2].view((1, self.num_anchors, 1, 1))
 
     def forward(self, x, targets=None, img_dim=None):
+
         # Tensors for cuda support
-        if x.is_cuda:
-            FloatTensor = torch.cuda.FloatTensor
-        else:
-            FloatTensor = torch.FloatTensor
+        FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
+        LongTensor = torch.cuda.LongTensor if x.is_cuda else torch.LongTensor
+        ByteTensor = torch.cuda.ByteTensor if x.is_cuda else torch.ByteTensor
 
         self.img_dim = img_dim
-        num_samples = x.shape[0]
-        grid_size = x.shape[2]
+        num_samples = x.size(0)
+        grid_size = x.size(2)
 
-        stride, grid_x, grid_y, scaled_anchors, anchor_w, anchor_h = self.compute_grid_offsets(grid_size, cuda=x.is_cuda)
-
-        # reshape the data andra transpose some rows
         prediction = (
             x.view(num_samples, self.num_anchors, self.num_classes + 5, grid_size, grid_size)
             .permute(0, 1, 3, 4, 2)
             .contiguous()
         )
 
+        # Get outputs
         x = torch.sigmoid(prediction[..., 0])  # Center x
         y = torch.sigmoid(prediction[..., 1])  # Center y
         w = prediction[..., 2]  # Width
         h = prediction[..., 3]  # Height
-        pred_obj_conf = torch.sigmoid(prediction[..., 4])  # Object confidence
-        class_pred = torch.sigmoid(prediction[..., 5:])  # Class prediction
+        pred_conf = torch.sigmoid(prediction[..., 4])  # Conf
+        pred_cls = torch.sigmoid(prediction[..., 5:])  # Cls pred.
 
-        # Use formulas b_x = sigmoid(p_x) + c_x, b_y = sigmoid(p_y) + c_y, b_w = a_w exp(p_w), b_h = a_h exp(p_h),
-        # where b_x, b_y, b_w, b_h are the coordinates, width and height that will be the output
-        # and p_x, p_y, p_w, p_h are what we have called prediction
+        # If grid size does not match current we compute new offsets
+        print("Checking grid")
+        if grid_size != self.grid_size:
+            t1 = time.time()
+            self.compute_grid_offsets(grid_size, cuda=x.is_cuda)
+            t2 = time.time()
+            print("Time used for compute grid diff: ", t2-t1)
+
+        # Add offset and scale with anchors
         pred_boxes = FloatTensor(prediction[..., :4].shape)
-        pred_boxes[..., 0] = x.data + grid_x
-        pred_boxes[..., 1] = y.data + grid_y
-        pred_boxes[..., 2] = torch.exp(w.data) * anchor_w
-        pred_boxes[..., 3] = torch.exp(h.data) * anchor_h
+        pred_boxes[..., 0] = x.data + self.grid_x
+        pred_boxes[..., 1] = y.data + self.grid_y
+        pred_boxes[..., 2] = torch.exp(w.data) * self.anchor_w
+        pred_boxes[..., 3] = torch.exp(h.data) * self.anchor_h
 
         output = torch.cat(
             (
-                pred_boxes.view(num_samples, -1, 4) * stride,
-                pred_obj_conf.view(num_samples, -1, 1),
-                class_pred.view(num_samples, -1, self.num_classes),
+                pred_boxes.view(num_samples, -1, 4) * self.stride,
+                pred_conf.view(num_samples, -1, 1),
+                pred_cls.view(num_samples, -1, self.num_classes),
             ),
             -1,
         )
@@ -186,31 +185,30 @@ class YOLOLayer(nn.Module):
         if targets is None:
             return output, 0
         else:
-            # Get targets
             iou_scores, class_mask, obj_mask, noobj_mask, tx, ty, tw, th, tcls, tconf = build_targets(
                 pred_boxes=pred_boxes,
-                pred_cls=class_pred,
+                pred_cls=pred_cls,
                 target=targets,
-                anchors=scaled_anchors,
+                anchors=self.scaled_anchors,
                 ignore_thres=self.ignore_thres,
             )
 
             # Loss : Mask outputs to ignore non-existing objects (except with conf. loss)
-            loss_x = self.mae_loss(x[obj_mask], tx[obj_mask])
-            loss_y = self.mae_loss(y[obj_mask], ty[obj_mask])
-            loss_w = self.mae_loss(w[obj_mask], tw[obj_mask])
-            loss_h = self.mae_loss(h[obj_mask], th[obj_mask])
-            loss_obj_conf = self.bce_loss(pred_obj_conf[obj_mask], tconf[obj_mask])
-            loss_noobj_conf = self.bce_loss(pred_obj_conf[noobj_mask], tconf[noobj_mask])
-            loss_conf = self.obj_scale * loss_obj_conf + self.noobj_scale * loss_noobj_conf
-            loss_cls = self.bce_loss(class_pred[obj_mask], tcls[obj_mask])
+            loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
+            loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
+            loss_w = self.mse_loss(w[obj_mask], tw[obj_mask])
+            loss_h = self.mse_loss(h[obj_mask], th[obj_mask])
+            loss_conf_obj = self.bce_loss(pred_conf[obj_mask], tconf[obj_mask])
+            loss_conf_noobj = self.bce_loss(pred_conf[noobj_mask], tconf[noobj_mask])
+            loss_conf = self.obj_scale * loss_conf_obj + self.noobj_scale * loss_conf_noobj
+            loss_cls = self.bce_loss(pred_cls[obj_mask], tcls[obj_mask])
             total_loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
 
             # Metrics
             cls_acc = 100 * class_mask[obj_mask].mean()
-            conf_obj = pred_obj_conf[obj_mask].mean()
-            conf_noobj = pred_obj_conf[noobj_mask].mean()
-            conf50 = (pred_obj_conf > 0.5).float()
+            conf_obj = pred_conf[obj_mask].mean()
+            conf_noobj = pred_conf[noobj_mask].mean()
+            conf50 = (pred_conf > 0.5).float()
             iou50 = (iou_scores > 0.5).float()
             iou75 = (iou_scores > 0.75).float()
             detected_mask = conf50 * class_mask * tconf
