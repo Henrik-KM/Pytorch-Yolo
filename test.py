da6ffb03cd43e7ac35a41428b1ca98a7fc0bd6f7
Ccx55 <ccx555@gmail.com>
2020-11-20 12:11:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
         targets[:, 2:] = xywh2xyxy(targets[:, 2:])
         targets[:, 2:] *= img_size
 
-        if len(imgs) == opt.batch_size:
+        if len(imgs) == batch_size:
             imgs = torch.stack(imgs)
         imgs = Variable(imgs.type(Tensor), requires_grad=False)
 
