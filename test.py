6d1658cfbff0e785794bd2d29624dc40409d85c4
Ccx55 <ccx555@gmail.com>
2020-11-20 09:06:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 from models import *
 from utils.utils import *
-from utils.datasets import *
+from utils.datasetsNSM import *
 from utils.parse_config import *
 
 import os
